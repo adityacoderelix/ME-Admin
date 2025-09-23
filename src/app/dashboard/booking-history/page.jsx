097d036f7dc265424c0aa786b2157b18d5aaef64
aditya <adityaparuleker.cr@gmail.com>
2025-09-23 16:56:02 +0530

    ME-A015 : Booking List, User Profile

--- src/app/dashboard/booking-history/page.jsx
+++ src/app/dashboard/booking-history/page.jsx
@@ -18,6 +18,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -47,6 +48,7 @@ import {
 import { Checkbox } from "@/components/ui/checkbox";
 import {
   CalendarIcon,
+  Star,
   Download,
   Filter,
   MoreHorizontal,
@@ -65,31 +67,9 @@ import { Calendar } from "@/components/ui/calendar";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
-// Added booking entry based on your provided details.
-// const bookings = [
-//   {
-//     id: "booking_1",
-//     guest: "Divya Yash",
-//     property: "Listing for Goa",
-//     checkIn: "24 March 2025",
-//     checkOut: "28 March 2025",
-//     total: "₹1,37,025",
-//     status: "Confirmed",
-//   },
-// ];
 const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export default function BookingsPage() {
-  const [bookings, setBookings] = React.useState([
-    // {
-    //   id: "booking_1",
-    //   guest: "Divya Yash",
-    //   property: "Listing for Goa",
-    //   checkIn: "24 March 2025",
-    //   checkOut: "28 March 2025",
-    //   total: "₹1,37,025",
-    //   status: "Pending",
-    // },
-  ]);
+  const [bookings, setBookings] = React.useState([]);
   const router = useRouter();
   const [date, setDate] = React.useState({
     from: addMonths(new Date(), -1),
@@ -101,24 +81,52 @@ export default function BookingsPage() {
   const [loading, setLoading] = React.useState(true);
   const [userEmail, setUserEmail] = React.useState();
   const [rejectDialogOpen, setRejectDialogOpen] = React.useState(false);
+  const [hostSearch, setHostSearch] = React.useState("");
   const [bookingId, setBookingId] = React.useState(null);
+  const [selectHost, setSelectHost] = React.useState("all");
+  const [hostEmail, setHostEmail] = React.useState([]);
   // Simulate a 2 second loading delay to show the skeleton UI.
   const getDate = (item) => {
     const d = new Date(item);
     d.setUTCHours(0, 0, 0, 0);
     return d.toISOString();
   };
+  const fetchHostEmails = async () => {
+    const getLocalData = await localStorage.getItem("admin");
+    const data = JSON.parse(getLocalData);
+
+    if (data) {
+      try {
+        const response = await fetch(`${API_URL}/booking/hostEmails`, {
+          method: "GET",
+          headers: {
+            //   Authorization: `Bearer ${data}`,
+            "Content-Type": "application/json",
+          },
+        });
+        const result = await response.json();
+        console.log("sssss", result);
+        const final = await result.data;
+        setHostEmail(final);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+  React.useEffect(() => {
+    fetchHostEmails();
+  }, []);
   const fetchData = async () => {
     const getLocalData = await localStorage.getItem("token");
     const data = JSON.parse(getLocalData);
 
-    const from = date?.from ? getDate(date.from) : null;
-    const to = date?.to ? getDate(date.to) : null;
+    const from = date?.from ? new Date(date.from).toLocaleDateString() : null;
+    const to = date?.to ? new Date(date.to).toLocaleDateString() : null;
     console.log("here", from);
     if (data) {
       try {
         const response = await fetch(
-          `${API_URL}/booking/analytics-filter?search=${searchTerm}&status=${activeTab}&from=${from}&to=${to}`,
+          `${API_URL}/booking/users-by-host?hostId=${selectHost}&search=${searchTerm}&from=${from}&to=${to}`,
           {
             method: "GET",
             headers: {
@@ -135,7 +143,7 @@ export default function BookingsPage() {
           return;
         }
         const result = await response.json();
-        console.log("data ext", result.data);
+        console.log("data ext", result);
         const final = await result.data;
         setBookings(final);
       } catch (err) {
@@ -145,7 +153,7 @@ export default function BookingsPage() {
   };
   React.useEffect(() => {
     fetchData();
-  }, [searchTerm, activeTab, date]);
+  }, [searchTerm, selectHost, date]);
 
   React.useEffect(() => {
     const timer = setTimeout(() => {
@@ -153,137 +161,6 @@ export default function BookingsPage() {
     }, 2000);
     return () => clearTimeout(timer);
   }, []);
-  // React.useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const bookingData = await fetch(`${API_URL}/booking/`, {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       });
-  //       const result = await bookingData.json();
-  //       for (let i in result.data) {
-  //         setUserEmail(result.data[i].userId.email);
-  //         const checkInDate = new Date(result.data[i].checkIn);
-  //         const checkOutDate = new Date(result.data[i].checkOut);
-  //         const monthNames = [
-  //           "January",
-  //           "February",
-  //           "March",
-  //           "April",
-  //           "May",
-  //           "June",
-  //           "July",
-  //           "August",
-  //           "September",
-  //           "October",
-  //           "November",
-  //           "December",
-  //         ];
-
-  //         const checkInYear = checkInDate.getFullYear();
-  //         const checkOutYear = checkOutDate.getFullYear();
-  //         const checkInMonth = checkInDate.getMonth();
-  //         const checkOutMonth = checkOutDate.getMonth();
-  //         const checkInMonthName = monthNames[checkInMonth]; // This will return "March"
-  //         const checkOutMonthName = monthNames[checkOutMonth];
-  //         const checkInDay = checkInDate.getDate();
-  //         const checkOutDay = checkOutDate.getDate();
-  //         const data = {
-  //           checkIn: checkInDay + " " + checkInMonthName + " " + checkInYear,
-  //           checkOut:
-  //             checkOutDay + " " + checkOutMonthName + " " + checkOutYear,
-  //           guest:
-  //             result.data[i].hostId.firstName +
-  //             " " +
-  //             result.data[i].hostId.lastName,
-  //           id: result.data[i]._id,
-  //           property: result.data[i].propertyId.title,
-  //           status: result.data[i].status,
-  //           total: `₹ ${result.data[i].price}`,
-  //         };
-
-  //         bookings.push(data);
-  //       }
-  //       setBookings(bookings);
-  //       return result;
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-  console.log("new", bookings);
-  console.log("tab", activeTab);
-  console.log("date", date);
-  // const filteredBookings = bookings.filter(
-  //   (booking) =>
-  //     (booking.guest.toLowerCase().includes(searchTerm.toLowerCase()) ||
-  //       booking.property.toLowerCase().includes(searchTerm.toLowerCase())) &&
-  //     (activeTab === "all" ||
-  //       booking.status.toLowerCase() === activeTab.toLowerCase())
-  // );
-
-  const toggleBookingSelection = (id) => {
-    setSelectedBookings((prev) =>
-      prev.includes(id) ? prev.filter((b) => b !== id) : [...prev, id]
-    );
-  };
-
-  const handleBulkAction = (action) => {
-    console.log(`Performing ${action} on bookings:`, selectedBookings);
-
-    // Implement bulk action logic here
-  };
-  // const sendConfirmationToUser = async (bookingId, userEmail) => {
-  //   try {
-  //     const response = await fetch(`${API_URL}/booking/host/confirm`, {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         bookingId: bookingId,
-  //         userEmail: userEmail,
-  //       }),
-  //     });
-  //     // renderBookingTable();
-  //     return response;
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
-  const sendRejectionToUser = async () => {
-    try {
-      console.log("nn", bookingId);
-      const getLocalData = await localStorage.getItem("token");
-      const data = JSON.parse(getLocalData);
-
-      const response = await fetch(`${API_URL}/booking/admin/cancel`, {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${data}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          bookingId: bookingId,
-        }),
-      });
-      if (!response.status == 200) {
-        return;
-      }
-
-      setBookingId(null);
-      setRejectDialogOpen(false);
-      toast.success("Booking successfully cancelled");
-      fetchData();
-      return response;
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   const renderBookingTable = (bookings) => {
     if (loading) {
@@ -349,68 +226,54 @@ export default function BookingsPage() {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[50px]">
-              {/* <Checkbox
-                checked={selectedBookings?.length === bookings?.length}
-                onCheckedChange={(checked) => {
-                  if (checked) {
-                    setSelectedBookings(bookings.map((b) => b.id));
-                  } else {
-                    setSelectedBookings([]);
-                  }
-                }}
-              /> */}
-            </TableHead>
             <TableHead className="w-[180px]">Guest</TableHead>
-            <TableHead className="w-[180px]">Property</TableHead>
-            <TableHead>Check-in</TableHead>
-            <TableHead>Check-out</TableHead>
+            <TableHead className="w-[180px]">Total Amount</TableHead>
+            <TableHead>Total Bookings</TableHead>
+            <TableHead>Total Reviews</TableHead>
             <TableHead>
               <Button variant="ghost" className="p-0 hover:bg-transparent">
-                <span>Total</span>
-                <SortAsc className="ml-2 h-4 w-4" />
+                <span>Rating</span>
+                {/* <SortAsc className="ml-2 h-4 w-4" /> */}
               </Button>
             </TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
+            {/* <TableHead>Status</TableHead>
+            <TableHead className="text-right">Actions</TableHead> */}
           </TableRow>
         </TableHeader>
         <TableBody>
           {bookings.map((booking) => (
             <TableRow key={booking._id}>
-              <TableCell>
-                <Checkbox
-                  checked={selectedBookings.includes(booking._id)}
-                  onCheckedChange={() => toggleBookingSelection(booking._id)}
-                />
-              </TableCell>
               <TableCell className="font-medium">
                 <span
                   title={
-                    booking.userId.firstName + " " + booking?.userId?.lastName
+                    booking.userId?.firstName + " " + booking?.userId?.lastName
                   }
+                  onClick={() =>
+                    router.push(
+                      `/dashboard/booking-history/user-profile?userId=${booking.userId._id}`
+                    )
+                  }
+                  className="underline cursor-pointer"
                 >
                   {checkLength(
-                    booking.userId.firstName + " " + booking?.userId?.lastName
+                    booking.userId?.firstName + " " + booking?.userId?.lastName
                   )}
                 </span>
               </TableCell>
               <TableCell>
-                <span title={booking?.propertyId.title}>
-                  {checkLength(booking?.propertyId.title)}
+                <span title={booking?.totalAmountSpent}>
+                  ₹ {booking?.totalAmountSpent}
                 </span>
               </TableCell>
-              <TableCell>
-                {new Date(booking?.checkIn).toDateString().slice(3)}
+              <TableCell>{booking?.totalBookings}</TableCell>
+              <TableCell>{booking?.totalReviews}</TableCell>
+              <TableCell className="flex">
+                {" "}
+                <Star className="h-4 w-4 text-yellow-400 ml-1" />{" "}
+                <span className="pl-2">{booking?.userId?.averageRating}</span>
               </TableCell>
-              <TableCell>
-                {new Date(booking?.checkOut).toDateString().slice(3)}
-              </TableCell>
-              <TableCell>{booking?.price}</TableCell>
-              <TableCell>
-                <StatusPill status={booking?.status} />
-              </TableCell>
-              <TableCell className="text-right">
+
+              {/* <TableCell className="text-right">
                 <DropdownMenu>
                   <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="h-8 w-8 p-0">
@@ -427,14 +290,14 @@ export default function BookingsPage() {
                     >
                       View details
                     </DropdownMenuItem>
-                    {/* <DropdownMenuItem>Modify booking</DropdownMenuItem>
+                    <DropdownMenuItem>Modify booking</DropdownMenuItem>
                     <DropdownMenuItem
                       onClick={() => {
                         sendConfirmationToUser(booking.id, userEmail);
                       }}
                     >
                       Send message
-                    </DropdownMenuItem> */}
+                    </DropdownMenuItem>
                     <DropdownMenuSeparator />
                     {booking.status != "rejected" &&
                     booking.status != "cancelled" ? (
@@ -449,7 +312,7 @@ export default function BookingsPage() {
                     ) : null}
                   </DropdownMenuContent>
                 </DropdownMenu>
-              </TableCell>
+              </TableCell> */}
             </TableRow>
           ))}
         </TableBody>
@@ -460,17 +323,23 @@ export default function BookingsPage() {
     await sendRejectionToUser();
   };
   return (
-    <div className="flex-1 space-y-4 p-8 pt-6 bg-gray-200 min-h-screen">
-      <div className="flex items-center justify-between space-y-2">
+    <div
+      className={
+        loading
+          ? "flex-1 h-screen space-y-4 p-8 pt-6 bg-gray-200 min-h-screen"
+          : "flex-1 space-y-4 p-8 pt-6 bg-gray-200 min-h-screen"
+      }
+    >
+      <div className="lg:flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-semibold font-bricolage tracking-tight">
           Bookings
         </h2>
-        <div className="flex items-center space-x-2">
+        <div className="md:flex  items-center md:space-x-2">
           <Popover>
             <PopoverTrigger asChild>
               <Button
                 variant={"outline"}
-                className={`w-[280px] justify-start text-left font-normal ${
+                className={`w-full lg:w-[280px] justify-start text-left font-normal ${
                   !date && "text-muted-foreground"
                 }`}
               >
@@ -500,7 +369,7 @@ export default function BookingsPage() {
               />
             </PopoverContent>
           </Popover>
-          <Button className="bg-primaryGreen text-white hover:bg-brightGreen rounded-md">
+          <Button className="mt-4 w-full md:mt-0 bg-primaryGreen text-white hover:bg-brightGreen rounded-md">
             <Download className="mr-2 h-4 w-4" />
             Export CSV
           </Button>
@@ -528,30 +397,67 @@ export default function BookingsPage() {
           </DialogFooter>
         </DialogContent>
       </Dialog>
-      <div className="flex-1">
+      <div className="flex">
         <Label htmlFor="search" className="sr-only">
           Search reservations
         </Label>
-        <Input
-          id="search"
-          className="bg-white"
-          placeholder="Search by guest name or property..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        <div className="w-full">
+          <Input
+            id="search"
+            className="bg-white"
+            placeholder="Search by guest name"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="pl-2">
+          <Select value={selectHost} onValueChange={setSelectHost}>
+            <SelectTrigger className="w-full sm:w-[250px] md:w-[380px] box-border bg-white">
+              <SelectValue placeholder="Host Email" />
+            </SelectTrigger>
+            <SelectContent className="w-full sm:w-[250px] md:w-[380px] max-w-[calc(100vw-2rem)] bg-white ">
+              <div className="overflow-x-scroll">
+                <div className="py-2 px-1">
+                  <Input
+                    placeholder="Search host email..."
+                    value={hostSearch}
+                    onChange={(e) => setHostSearch(e.target.value)}
+                    className="w-full"
+                    onKeyDown={(e) => e.stopPropagation()}
+                  />
+                </div>
+
+                <SelectItem value="all" selected>
+                  All
+                </SelectItem>
+                {hostEmail
+                  ?.filter((item) =>
+                    item?.hostEmail
+                      ?.toLowerCase()
+                      .includes(hostSearch?.toLowerCase())
+                  )
+                  .map((item) => (
+                    <SelectItem value={item?.host}>
+                      {item?.hostEmail}
+                    </SelectItem>
+                  ))}
+              </div>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
       <Tabs
         defaultValue="all"
         className="space-y-4"
         onValueChange={setActiveTab}
       >
-        <TabsList>
+        {/* <TabsList>
           <TabsTrigger value="all">All Bookings</TabsTrigger>
           <TabsTrigger value="confirmed">Confirmed</TabsTrigger>
           <TabsTrigger value="pending">Pending</TabsTrigger>
           <TabsTrigger value="rejected">Rejected</TabsTrigger>
           <TabsTrigger value="cancelled">Cancelled</TabsTrigger>
-        </TabsList>
+        </TabsList> */}
         {/* <div className="flex items-center space-x-2">
           <div className="flex-1">
             <div className="relative">
