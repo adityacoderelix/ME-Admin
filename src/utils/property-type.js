66d09fc80eff83735a50a1c21861a0f2481e2524
aditya <adityaparuleker.cr@gmail.com>
2025-10-27 12:29:20 +0530

    Hardcoded property

---
+++ src/utils/property-type.js
@@ -0,0 +1,105 @@
+export const properties = [
+  {
+    icon: "/images/property-icons/h.png",
+    label: "House",
+    route: "house",
+  },
+  {
+    icon: "/images/property-icons/guest.png",
+    label: "Guest House",
+    route: "guesthouse",
+  },
+  {
+    icon: "/images/property-icons/cottage.png",
+    label: "Cottage",
+    route: "cottage",
+  },
+  {
+    icon: "/images/property-icons/flats.svg",
+    label: "Hotel",
+    route: "hotel",
+  },
+  {
+    icon: "/images/property-icons/bungalow.png",
+    label: "Bungalow",
+    route: "bungalow",
+  },
+
+  {
+    icon: "/images/property-icons/farmhouse.svg",
+    label: "Farm House",
+    route: "farmhouse",
+  },
+  {
+    icon: "/images/property-icons/villa.svg",
+    label: "Villas",
+    route: "villa",
+  },
+
+  {
+    icon: "/images/property-icons/condo.png",
+    label: "Condo",
+    route: "condo",
+  },
+  {
+    icon: "/images/property-icons/houseboat.png",
+    label: "House Boat",
+    route: "houseboat",
+  },
+  {
+    icon: "/images/property-icons/yurt.png",
+    label: "Yurt",
+    route: "yurt",
+  },
+  {
+    icon: "/images/property-icons/apartment.svg",
+    label: "Apartments",
+    route: "apartment",
+  },
+  {
+    icon: "/images/property-icons/cabin.svg",
+    label: "Cabin",
+    route: "cabin",
+  },
+
+  // {
+  //   icon: "/images/property-icons/pool.svg",
+  //   label: "Pool",
+  //   route: "farm-house",
+  // },
+  {
+    icon: "/images/property-icons/trending.svg",
+    label: "Trending",
+    route: "farm-house",
+  },
+  // {
+  //   icon: "/images/property-icons/bed-and-breakfast.svg",
+  //   label: "Bed & Breakfast",
+  //   route: "farm-house",
+  // },
+  // {
+  //   icon: "/images/property-icons/rooms.svg",
+  //   label: "Rooms",
+  //   route: "farm-house",
+  // },
+  // {
+  //   icon: "/images/property-icons/beach.svg",
+  //   label: "Beach",
+  //   route: "farm-house",
+  // },
+  {
+    icon: "/images/property-icons/mansion.svg",
+    label: "Town House",
+    route: "townhouse",
+  },
+  {
+    icon: "/images/property-icons/historical-home.svg",
+    label: "Historical Home",
+    route: "farmhouse",
+  },
+  {
+    icon: "/images/property-icons/treehouse.svg",
+    label: "Tree House",
+    route: "treehouse",
+  },
+];
