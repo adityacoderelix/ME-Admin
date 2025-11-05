79e15aa78812c449977166d5e41272d10e849e1a
aditya <adityaparuleker.cr@gmail.com>
2025-11-05 16:56:08 +0530

    commit9

--- src/app/dashboard/view-property/page.jsx
+++ src/app/dashboard/view-property/page.jsx
@@ -1,8 +1,6 @@
 "use client";
 
 import React, { Suspense } from "react";
-import BookingsPage from "../page";
-import HostProfilePage from "./host-profile-page";
 import DetailView from "./detail-view";
 
 export default function Page() {
